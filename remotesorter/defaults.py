67074820b852dd078469242fbf5652efa8b02e3a
Hunter Jones <hjones2199@outlook.com>
2018-11-06 15:43:57 -0600

    Added defaults file that configures client and server default behavior.

---
+++ remotesorter/defaults.py
@@ -0,0 +1,14 @@
+"""
+Default values for the remotesorter program, used by both client
+and server to define the protocol's default behavior. Basically
+acts as a configuration file.
+"""
+ip_addr = "127.0.0.1"
+port_num = "43802"
+tcp_buffsize = 4096
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
